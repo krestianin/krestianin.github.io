7049e34ea3ca87e404e04d3a6dae4c03c2a34185
Ivan Postolov <43872166+krestianin@users.noreply.github.com>
2026-02-18 14:02:19 -0800

    added a 3d fitting proj  and images size fixed

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import tcp from '../assets/images/tcp.png';
 import controller from '../assets/images/controller.png';
 import floppydisk from '../assets/images/floppy-disk.png';
+import hand from '../assets/images/hand.png';
 import hex from '../assets/images/hex.png';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Project.scss';
@@ -14,6 +15,9 @@ function Project() {
         'C#', 'Unity', 'NavMesh', 'Animator'];
     const labelsSecond = [
         'C', 'Memory Management', 'Data Structures', 'AVL Trees'
+    ];
+    const labels3D = [
+        'Neural Netowrks', 'PointNet', 'Chamfer Distance', 'Primitive Fitting'
     ];
     const labelsThird = [
         'Python', 'Networking', 'UDP', 'TCP', 'Multithreading', 'Socket Programming'
@@ -34,7 +38,7 @@ function Project() {
                         <Chip key={index} className='chip' label={label} />
                     ))}
                 </div>
-           </div>
+            </div>
             <div className="project">
                 <a href="https://ivanpostolov.itch.io/fight-vs-zombies" target="_blank" rel="noreferrer"><img src={controller} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://ivanpostolov.itch.io/fight-vs-zombies" target="_blank" rel="noreferrer"><h2>Guy vs. Zombies</h2></a>
@@ -42,14 +46,25 @@ function Project() {
                     and off‑mesh traversal via a bespoke AgentLinkMover, a CharacterController‑based movement and camera rig, animation hooks, 
                     health and melee combat, and ground‑aware resource spawning. Core loop: fight zombies, collect coins, and restore health at
                     a base with a 1:5 coin‑to‑HP ratio.</p>
-                    <a href="https://github.com/krestianin/CasualGame" target="_blank" rel="noreferrer"><h3>GitHub link</h3></a>
+                    <a href="https://github.com/krestianin/CasualGame" target="_blank" rel="noreferrer"><h2>GitHub link</h2></a>
                 <div className="flex-chips">
                     <span className="chip-title">Tech stack:</span>
                     {labelscasual.map((label, index) => (
                         <Chip key={index} className='chip' label={label} />
                     ))}
                 </div>
-             </div>
+            </div>
+            <div className="project">
+                <a href="https://github.com/Jaewon0407/Neural-Primitive-Fitting-for-3D-models" target="_blank" rel="noreferrer"><img src={hand} className="zoom" alt="thumbnail"/></a>
+                <a href="https://github.com/Jaewon0407/Neural-Primitive-Fitting-for-3D-models" target="_blank" rel="noreferrer"><h2>3D Shape Parametric Representation</h2></a>
+                <p>Approximate complex 3D geometry using a small set of simple parametric shapes via a PointNet-based architecture</p>
+                  <div className="flex-chips">
+                    <span className="chip-title">Tech stack:</span>
+                    {labels3D.map((label, index) => (
+                        <Chip key={index} className='chip' label={label} />
+                    ))}
+                </div>
+            </div>
             <div className="project">
                 <a href="https://github.com/krestianin/MyMalloc/tree/main" target="_blank" rel="noreferrer"><img src={floppydisk} className="zoom" alt="thumbnail"/></a>
                 <a href="https://github.com/krestianin/MyMalloc/tree/main" target="_blank" rel="noreferrer"><h2>Custom Memory Allocator</h2></a>
@@ -62,7 +77,7 @@ function Project() {
                 </div>
             </div>
             <div className="project">
-                <a href="https://github.com/sosokokos/Proxy-Server-CMPT371" target="_blank" rel="noreferrer"><img src={tcp} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/sosokokos/Proxy-Server-CMPT371" target="_blank" rel="noreferrer"><img src={tcp} className="zoom" alt="thumbnail"/></a>
                 <a href="https://github.com/sosokokos/Proxy-Server-CMPT371" target="_blank" rel="noreferrer"><h2>Web Server</h2></a>
                 <p>Designed and implemented a high-performance, multi-threaded web server with caching and proxy capabilities in Python.</p>
                   <div className="flex-chips">
